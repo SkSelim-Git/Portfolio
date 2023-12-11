18ffc1824d9dfde36d8bb0aa8e914c7865df24e5
SkSelim-Git <skselim0777@gmail.com>
2023-12-12 00:52:26 +0530

    add all files in portfolio

---
+++ app.js
@@ -0,0 +1,45 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controlls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions(){
+    // Button click active class
+    for(let i=0; i< secBtn.length; i++){
+        secBtn[i].addEventListener('click',function(e){
+            let currentBtn = document.querySelectorAll('.active-btn');
+                currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+                this.className += 'active-btn';  
+        })
+    }
+}
+
+// Sections Active
+allSections.addEventListener('click', (e)=>{
+    const id = e.target.dataset.id;
+    if(id){
+        // remove selected from the other btns
+        secBtns.forEach((btn)=>{
+            btn.classList.remove('active')
+        })
+        e.target.classList.add('active')
+
+        // hide other sections
+        sections.forEach((section)=>{
+            section.classList.remove('active')
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add('active')
+    }
+})
+
+// Toggle Theme
+
+const themBtn = document.querySelector('.theme-btn');
+themBtn.addEventListener('click',()=>{
+    let element = document.body;
+    element.classList.toggle('light-mode')
+})
+
+PageTransitions();
